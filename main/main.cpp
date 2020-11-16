e5bcadcbe6549f06f489a22d5b893f5e2a8fb196
M1ND228 <dimitry.brazhnik@yandex.ru>
2020-11-16 14:13:14 +0300

    Work for function

--- main/main.cpp
+++ main/main.cpp
@@ -1,9 +1,29 @@
 #include <stdio.h>
-#include "shooter.h"
+#include "stack.h"
+#include "multistack.h"
+#include "multistackroot .h"
 
 int main() {
-  printf("hello!\n");
-  
-
+	TStack t1;
+	TStack t2;
+	
+	for (int i = 0; i < 5; i++)
+	{
+		t1.Put(i * 2);
+		t2.Put(i + 1);
+	}
+	t2.Put(10);
+	
+	t1.print();
+	std::cout << "\n";
+	t2.print();
+	std::cout << "\n";
+	std::cout << "\n";
+	t1.swap(t1, t2);
+	
+	t1.print();
+	std::cout << "\n";
+	t2.print();
+	
   return 0;
 }
