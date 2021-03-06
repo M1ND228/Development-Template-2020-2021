6f9b518971894d24c21749c5cc8516a95303a94b
M1ND228 <dimity.brazhnik@yandex.ru>
2021-03-06 16:59:10 +0300

    Changed bug if invalid data input or uncorrected. Added new function checking input data. Input overload works correctly. Changed the conversion function from Roman to Arabic

--- function_lib/Converter.h
+++ function_lib/Converter.h
@@ -26,13 +26,14 @@ public:
 	arabien ConvertRomToArab(roman R_);
 	roman	ConvertArabToRom(arabien A_);
 
-	friend std::ostream& operator<< (std::ostream& out, const Converter& converter);
-	friend std::istream& operator>> (std::istream& in, Converter& converter);
-
 	void OutputConsole();
-	
+	int CheckInput(std::string inText);
+
 	std::string Get_R_Val();
 	int			Get_A_Val();
+
+	friend std::ostream& operator<< (std::ostream& out, const Converter& converter);
+	friend std::istream& operator>> (std::istream& in, Converter& converter);
 
 private:
 	roman	R;
@@ -47,8 +48,30 @@ inline Converter::Converter()
 
 inline Converter::Converter(std::string R_)
 {
-	R.val = R_;
-	A = ConvertRomToArab(R);
+	int num = 0;
+	switch (CheckInput(R_))
+	{
+	case 1:
+		R.val = R_;
+		A = ConvertRomToArab(R);
+		R = ConvertArabToRom(A);		// Edit input Roman Line to correct 
+		break;
+	case 0:
+		num = atoi(R_.c_str());
+		if (num < 0 || num > 3999)
+		{
+			throw std::out_of_range("Input error: The number is out of bounds");
+		}
+		A.val = num;
+		R = ConvertArabToRom(A);
+		break;
+	case -1:
+		throw std::logic_error("Logic error: Invalid data.");
+		break;
+	default:
+		throw std::logic_error("Logic error: Invalid data.");
+		break;
+	}
 }
 
 inline Converter::Converter(int A_)
@@ -67,30 +90,43 @@ inline Converter::~Converter()
 
 inline arabien Converter::ConvertRomToArab(roman R_)
 {
-	int const  arab_[] = { 1,   4,    5,   9,    10,  40,  50,   90,  100, 400,  500, 900,  1000 };
-	char const* roman_[] = { "I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M" };
+	int Sum = 0;
 
-	std::string num = R.val;
+	int sizeText = R_.val.size();
+	std::string inText = R_.val;
 
-	const int m = countof(arab_) - 1;
-	int arab = 0;
-	int len = num.length(), n = m, i = 0, pir;
-
-	while (n >= 0 && i < len)
-	{
-		pir = n & 1;
-		if ((num[i] == roman_[n][0]) && (!pir || num[i + 1] == roman_[n][1]))
-		{
-			arab += arab_[n];
-			i += 1 + pir;
-		}
-		else n--;
-	}
+	int  const  arab_[] = { 1000, 900, 500, 400,  100,  90,  50,   40,   10,   9,   5,   4,    1 };
+	char const* roman_[] = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
 
 	arabien res;
-	res.val = arab;
-	R = ConvertArabToRom(res);
+	res.val = 0;
+
+	for (int i = 0; i < sizeText; i++)
+	{
+		for (int j = 0; j < countof(roman_); j++)
+		{
+			if (inText[i] == roman_[j][0])
+			{
+				if ((inText[i + 1] == roman_[j][1]) && (i != sizeText - 1))
+				{
+					Sum += arab_[j];
+					i++;
+					break;
+				}
+				else
+				{
+					if ((j % 2 == 0) || j == 12)
+					{
+						Sum += arab_[j];
+						break;
+					}
+				}
+			}
+		}
+	}
 	
+	res.val = Sum;
+
 	return res;
 }
 
@@ -99,11 +135,12 @@ inline roman Converter::ConvertArabToRom(arabien A_)
 {
 	int num = A_.val;
 	roman res;
+	res.val = "";
 
 	int roman_value_list[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
 	std::string roman_char_list[] = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
 
-	for (int i = 0; i < 13; i++) 
+	for (int i = 0; i < countof(roman_value_list); i++)
 	{
 		while (num >= roman_value_list[i]) 
 		{
@@ -123,10 +160,28 @@ std::ostream& operator<<(std::ostream& out, const Converter& converter)
 
 inline std::istream& operator>>(std::istream& in, Converter& converter)
 {
-	std::cout << "Input roman: ";
-	in >> converter.R.val;
-	std::cout << "Input arab: ";
-	in >> converter.A.val;
+	std::string inText = "";
+	std::cout << "Input number: ";
+	in >> inText;
+
+	switch (converter.CheckInput(inText))
+	{
+	case 0:
+		converter.A.val = atoi(inText.c_str());
+	 	converter.R = converter.ConvertArabToRom(converter.A);
+		break;
+	case 1:
+		converter.R.val = inText;
+		converter.A = converter.ConvertRomToArab(converter.R);
+		converter.R = converter.ConvertArabToRom(converter.A);
+		break;
+	case -1:
+		throw std::logic_error("Logic error: Invalid data.");
+		break;
+	
+	default:
+		break;
+	}
 
 	return in;
 }
@@ -134,6 +189,35 @@ inline std::istream& operator>>(std::istream& in, Converter& converter)
 inline void Converter::OutputConsole()
 {
 	std::cout << "Roman - " << R.val << " Arab - " << A.val << "\n";
+}
+
+inline int Converter::CheckInput(std::string inText)
+{
+	// 0 - Арабские, 1 - Римские, -1 - Ошибка
+	int codeOut = -1;
+
+	for each (int symCode in inText)
+	{
+		// 0...9
+		if (symCode >= 48 && symCode <= 57)
+		{
+			if (codeOut == 1) { return -1; }
+			codeOut = 0;
+		}
+		else {
+			// I V X L C D M
+			if (symCode == 73 || symCode == 86 || symCode == 88 || symCode == 76 || symCode == 67 || symCode == 68 || symCode == 77)
+			{
+				if (codeOut == 0) { return -1; }
+				codeOut = 1;
+			}
+			else
+			{
+				return -1;
+			}
+		}
+	}
+	return codeOut;
 }
 
 inline std::string Converter::Get_R_Val()
